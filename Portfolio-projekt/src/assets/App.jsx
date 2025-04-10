dbf62d99067ff2c33f78c08cc2d49a988f37e81e
WilleRonnK <willerkjellqvist@gmail.com>
2025-04-10 03:53:24 +0200

    added a type effect

--- Portfolio-projekt/src/assets/App.jsx
+++ Portfolio-projekt/src/assets/App.jsx
@@ -1,22 +1,51 @@
 import "./App.css";
 import { Link } from "react-scroll";
+import { FaHome, FaBook, FaPaperPlane } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
 
 function App() {
   const menuItems = [
     {
       id: 1,
       title: "home",
+      icon: <FaHome />,
+      Image: 'https://i.sstatic.net/jGlzr.png'
     },
     {
       id: 2,
       title: "portfolio",
+      icon: <FaBook />,
     },
     {
       id: 3,
       title: "contact",
+      icon: <FaPaperPlane />
     },
-   
   ];
+
+  const useTypewriter = (text, speed = 50) => {
+    const [displayText, setDisplayText] = useState('');
+
+    useEffect(() => {
+      let i = 0;
+      const typingInterval = setInterval(() => {
+        if (i < text.length) {
+          setDisplayText(prevText => prevText + text.charAt(i));
+          i++;
+        } else {
+          clearInterval(typingInterval);
+        }
+      }, speed);
+
+      return () => {
+        clearInterval(typingInterval);
+      };
+    }, [text, speed]);
+
+    return displayText;
+  };
+  const homeText = useTypewriter("  Hi, my name is Wille.");
+  const homeDescription = useTypewriter("  I'm a Software Developer");
 
   return (
     <div className="App">
@@ -26,14 +55,15 @@ function App() {
 
           <ul>
             {menuItems.map((menu) => (
-              <li>
+              <li key={menu.id}>
                 <Link
                   to={menu.title}
                   smooth={true}
-                  offset={-560}
+                  offset={-80}
                   duration={1}
                 >
                   {menu.title}
+                  {menu.icon && <span className="menu-icon">{menu.icon}</span>}
                 </Link>
               </li>
             ))}
@@ -43,10 +73,19 @@ function App() {
 
       <main>
         {menuItems.map((menu) => (
-          <div className="content">
-            <h1 className="content-header" id={menu.title}>
-              {menu.title}
-            </h1>
+          <div
+            key={menu.id}
+            className={`content ${menu.title === "home" ? "home-section" : ""}`}
+            id={menu.title}
+          >
+            {menu.title === "home" ? (
+              <div className="home-text">
+                <h1>{homeText}</h1>
+                <p>{homeDescription}</p>
+              </div>
+            ) : (
+              <h1 className="content-header">{menu.title}</h1>
+            )}
           </div>
         ))}
       </main>
